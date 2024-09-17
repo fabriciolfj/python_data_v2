46e668bd1cdf4ccbd6b0b1b7b64ada216b4448de
spark <fabricio.jacob@outlook.com>
2024-09-17 15:01:20 -0300

    add

--- pandas_teste/pivotamento_formato_long_para_largo/test.py
+++ pandas_teste/pivotamento_formato_long_para_largo/test.py
@@ -5,19 +5,20 @@ import pandas as pd
 data = pd.read_csv("../../files/macrodata.csv")
 data = data.loc[:, ["year", "quarter", "realgdp", "infl", "unemp"]]
 
+print("=========normal")
 print(data.head())
-print("=========")
+print("=========periods")
 periods = pd.PeriodIndex.from_fields(year=data.pop("year"), quarter=data.pop("quarter"))
 print(periods)
-print("=========")
+print("=========index")
 data.index = periods.to_timestamp("D")
 print(data.head())
-print("=========")
+print("=========reindex")
 data = data.reindex(columns=["realgdp", "infl", "unemp"])
 data.columns.name = "item"
 data.index.name = "date"
 print(data.head())
-print("=========")
+print("========= stack")
 long_data = (data.stack()
 .reset_index().rename(columns={0: "value"}))                #reset_index transforma uma Series com multiindex em um dataframe
 
